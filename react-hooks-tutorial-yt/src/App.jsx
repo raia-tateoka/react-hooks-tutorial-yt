9be48ac4101358cfda4c6f1b86ccb0f755eb5f1c
R-Tateoka <tateoka@goodbee.jp>
2023-02-08 12:54:56 +0900

    useEffect

--- react-hooks-tutorial-yt/src/App.jsx
+++ react-hooks-tutorial-yt/src/App.jsx
@@ -1,6 +1,13 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
 
+/**
+ * useState   データが変更されたタイミングでレンダリングする
+ * useEffect  発火のタイミングを決めることができる
+ *            useEffect内で依存関係のあるものを使わない（無限ループ対策）
+ * 
+ * 
+ */
 function App() {
   const [count, setCount] = useState(0);
 
@@ -8,9 +15,13 @@ function App() {
     setCount(count + 1);
   };
 
+  useEffect(() => {
+    console.log("Hello, Hooks");
+  }, [count]);
+
   return (
     <div className="App">
-      <h1>UseState</h1>
+      <h1>UseState, UseEffect</h1>
       <button onClick={handleClick}>＋</button>
       <p>{count}</p>
     </div>
--- react-hooks-tutorial-yt/src/main.jsx
+++ react-hooks-tutorial-yt/src/main.jsx
@@ -4,6 +4,11 @@ import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  /**
+   * ＜useEffectについて＞
+   *  strictmodeでは厳格に実行するためuseEffectが2回発火する
+   *  本番モードでは1回のみ
+   */
   <React.StrictMode>
     <App />
   </React.StrictMode>,
