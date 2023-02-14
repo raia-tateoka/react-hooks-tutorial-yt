588816ac251dfaecff315d23c031e5ad218f89ef
_gorilla_ <t.workout.r@gmail.com>
2023-02-15 00:03:54 +0900

    useContext

--- react-hooks-tutorial-yt/src/App.jsx
+++ react-hooks-tutorial-yt/src/App.jsx
@@ -1,15 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import './App.css'
+import ShinCodeContext from './main';
 
 /**
  * useState   データが変更されたタイミングでレンダリングする
  * useEffect  発火のタイミングを決めることができる
  *            useEffect内で依存関係のあるものを使わない（無限ループ対策）
- * 
+ * useContext コンポーネント間のデータの受け渡し
  * 
  */
 function App() {
   const [count, setCount] = useState(0);
+  const shincodeInfo = useContext(ShinCodeContext);
 
   const handleClick = () => {
     setCount(count + 1);
@@ -24,6 +26,11 @@ function App() {
       <h1>UseState, UseEffect</h1>
       <button onClick={handleClick}>＋</button>
       <p>{count}</p>
+
+      <hr />
+      <h1>useContext</h1>
+      <p>{shincodeInfo.name}</p>
+      <p>{shincodeInfo.age}</p>
     </div>
   );
 }
--- react-hooks-tutorial-yt/src/main.jsx
+++ react-hooks-tutorial-yt/src/main.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
+import { createContext } from 'react';
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+
+const shincodeInfo = {
+  name: "shincode",
+  age: 24,
+};
+
+const ShinCodeContext = createContext(shincodeInfo);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   /**
    * ＜useEffectについて＞
    *  strictmodeでは厳格に実行するためuseEffectが2回発火する
    *  本番モードでは1回のみ
+   * ＜useContextについて＞
+   * Providerで囲われた中でデータを使うことができる
    */
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <ShinCodeContext.Provider value={shincodeInfo}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ShinCodeContext.Provider>
+);
+
+export default ShinCodeContext;
