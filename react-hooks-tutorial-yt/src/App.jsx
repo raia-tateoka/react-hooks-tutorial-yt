4220885943226b2be16467fc95e73376f73a2580
_gorilla_ <t.workout.r@gmail.com>
2023-02-17 22:37:36 +0900

    useReducer

--- react-hooks-tutorial-yt/src/App.jsx
+++ react-hooks-tutorial-yt/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useContext, useRef } from 'react';
+import { useEffect, useState, useContext, useRef, useReducer } from 'react';
 import './App.css'
 import ShinCodeContext from './main';
 
@@ -8,13 +8,26 @@ import ShinCodeContext from './main';
  *            useEffect内で依存関係のあるものを使わない（無限ループ対策）
  * useContext コンポーネント間のデータの受け渡し
  * useRef     input等の参照したい値を取得することができる
- * 
+ * useReducer Reduxのような概念（難しいため今はこんなものなんだ程度で覚える）
  * 
  */
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
 function App() {
   const [count, setCount] = useState(0);
   const shincodeInfo = useContext(ShinCodeContext);
   const ref = useRef();
+  const [state, dispatch] = useReducer(reducer, 0);
 
   const handleClick = () => {
     setCount(count + 1);
@@ -44,7 +57,12 @@ function App() {
       <h1>useRef</h1>
       <input type="text" ref={ref}/>
       <button onClick={handleRef}>UseRef</button>
-      
+
+      <hr />
+      <h1>useReducer</h1>
+      <p>カウント：{state}</p>
+      <button onClick={() => dispatch({ type: "increment" })}>＋</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>－</button>
     </div>
   );
 }
